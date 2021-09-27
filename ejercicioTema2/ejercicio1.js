9df24a99ca27eefc64820dfc63e95c1a4161bd24
Juan Carlos <jctr2002@gmail.com>
2021-09-27 10:58:47 +0200

    subida ejercicio1

---
+++ ejercicioTema2/ejercicio1.js
@@ -0,0 +1,8 @@
+var nombre = prompt("Introduce tu nombre:"); 
+var apellidos = prompt("Introduce tu apellido:");
+var dato1 = parseInt(prompt("Introduce el primer numero:"));
+var dato2 = parseInt(prompt("Introduce el segundo numero:"));
+var dato3 = parseInt(prompt("Introduce el tercer numero:"));
+
+alert("Tu usuario es : "+nombre+" "+apellidos);
+alert("Los numeros en orden de entrada son : "+dato1+" "+dato2+" "+dato3);
